7b9d4acabaa67e0d5637ba9a507cfdea1b7eea77
Richard W <wallin.90@hotmail.com>
2021-04-16 09:55:42 +0200

    Init project

---
+++ src/main/java/com/github/rw90/exjobb/MapApp/MapAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.rw90.exjobb.MapApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapAppApplication.class, args);
+	}
+
+}
