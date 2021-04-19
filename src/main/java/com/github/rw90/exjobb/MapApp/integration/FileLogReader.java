c9df9a62474660e324a18dfb1825b939db1cc196
Richard W <wallin.90@hotmail.com>
2021-04-19 10:20:39 +0200

    Finish initial vertical slice

--- src/main/java/com/github/rw90/exjobb/MapApp/integration/FileLogReader.java
+++ src/main/java/com/github/rw90/exjobb/MapApp/integration/FileLogReader.java
@@ -1,17 +1,26 @@
 package com.github.rw90.exjobb.MapApp.integration;
 
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.stream.Stream;
 
+@Component
 public class FileLogReader implements LogReader {
 
     @Override
-    public Flux<String> readAllLines() throws IOException {
-        Stream<String> logLines = Files.lines(Path.of("src/main/resources/startedservices.csv"));
-        return Flux.fromStream(logLines);
+    public Flux<String> readAllLines() {
+        Stream<String> logLines = null;
+        try {
+            logLines = Files.lines(Path.of("src/main/resources/startedservices.csv"));
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
+        return Flux.fromStream(logLines).delayElements(Duration.ofMillis(300));
     }
 }
--- src/main/java/com/github/rw90/exjobb/MapApp/service/LogService.java
+++ src/main/java/com/github/rw90/exjobb/MapApp/service/LogService.java
@@ -1,8 +1,19 @@
 package com.github.rw90.exjobb.MapApp.service;
 
 import com.github.rw90.exjobb.MapApp.integration.LogReader;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
+@Service
 public class LogService {
 
-    private LogReader
+    private final LogReader logReader;
+
+    public LogService(LogReader logReader) {
+        this.logReader = logReader;
+    }
+
+    public Flux<String> getLogLines() {
+        return logReader.readAllLines();
+    }
 }
