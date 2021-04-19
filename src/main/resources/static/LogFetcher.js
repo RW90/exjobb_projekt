03818847814be519676b5ac85d866361ed70a629
nedvin <43271582+nedvin@users.noreply.github.com>
2021-04-19 21:06:52 +0200

    Update LogFetcher.js

--- src/main/resources/static/LogFetcher.js
+++ src/main/resources/static/LogFetcher.js
@@ -1,4 +1,4 @@
-const eventSource = new EventSource("http://localhost:8080/logstream");
+const eventSource = new EventSource("/logstream");
 const container = document.getElementById("serviceNames");
 eventSource.onmessage = function ({data}){
     const serviceName = document.createElement("p");
